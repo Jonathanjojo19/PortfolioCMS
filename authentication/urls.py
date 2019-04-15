cc7f08897a918a3b25c87cf2741850b327ee00a5
Jonathanjojo <jonathanchristopher1199@gmail.com>
2019-04-15 20:49:46 +0700

    Finish auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "authentication"
 
 urlpatterns = [
+    path('def_login/', views.default_login_view, name="def_login"),
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 
+def default_login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if request.user.is_superuser or request.user.is_staff:
+                return redirect("portfolio:index")
+            else:
+                logout(request)
+        return render(request, "authentication/auth.html", {
+            "form" : AuthenticationForm(),
+            "feedback" : "Sorry, you are not authorized to edit the page",
+            "next" : reverse('authentication:def_login')
+        })
+    return render(request, "authentication/auth.html", {
+        "form" : AuthenticationForm(),
+        "feedback" : "",
+        "next" : reverse('authentication:def_login')
+    })
+
 def login_view(request):
-    response = {}
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -12,13 +32,18 @@ def login_view(request):
             if request.user.is_superuser or request.user.is_staff:
                 if "next" in request.POST:
                     return redirect(request.POST.get("next"))
-                return redirect("portfolio:index")
+                return redirect("cms:dashboard")
             else:
                 logout(request)
-        return redirect(request.POST.get("next"))
+        return render(request, "authentication/auth.html", {
+            "form" : AuthenticationForm(),
+            "feedback" : "Sorry, you are not authorized to edit the page",
+            "next" : reverse('authentication:login')
+        })
     return render(request, "authentication/auth.html", {
         "form" : AuthenticationForm(),
-        "feedback" : ""
+        "feedback" : "",
+        "next" : reverse('authentication:login')
     })
 
 def logout_view(request):
